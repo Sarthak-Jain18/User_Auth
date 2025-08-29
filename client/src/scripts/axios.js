053e3a2adc0776bdd3629eedb51e7a60caf6c01f
Sarthak Jain <jainsarthak1811@gmail.com>
2025-08-29 18:14:45 +0530

    Update axios.js

--- client/src/scripts/axios.js
+++ client/src/scripts/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-console.log("Backend URL:", import.meta.env.VITE_RENDER_URL);
+
 const api = axios.create({
     baseURL: import.meta.env.VITE_RENDER_URL,
     withCredentials: true,
